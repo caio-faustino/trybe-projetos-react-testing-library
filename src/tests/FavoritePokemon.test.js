48c7919aa5248ba8430540200acfbe2f64684640
caio-faustino <caioexfaustino.pro@gmail.com>
2022-12-14 12:35:11 -0300

    requisito 03

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <FavoritePokemon.js />.', () => {
+  it('Testa se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos.', () => {
+    // Acessar os elementos da Tela
+    const { history } = renderWithRouter(<App />);
+    // Interagir com os elementos
+    act(() => {
+      history.push('/favorites');
+    });
+
+    const favoriteText = screen.getByRole('heading', { name: 'Favorite Pokémon' });
+    const p1 = screen.getByText('No favorite Pokémon found');
+    // Fazer os testes
+    expect(favoriteText).toBeInTheDocument();
+    expect(p1).toBeInTheDocument();
+  });
+});
